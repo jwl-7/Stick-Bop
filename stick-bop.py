74dde0bff25694758c7400657d05ad6a5911e25c
jwl-7 <lucidrainbows@gmail.com>
2018-12-19 01:40:28 -0500

    added directory folders and paths

--- stick-bop.py
+++ stick-bop.py
@@ -1,9 +1,11 @@
 import pygame
 import random
-import os
+from os import path
 
-# look in dir that python file is in
-os.chdir(os.path.dirname(__file__))
+# directory paths
+img_dir  = path.join(path.dirname(__file__), 'images')
+snd_dir  = path.join(path.dirname(__file__), 'sounds')
+font_dir = path.join(path.dirname(__file__), 'fonts')
 
 SCREEN_WIDTH  = 900
 SCREEN_HEIGHT = 700
@@ -29,7 +31,7 @@ def main():
     pygame.display.set_caption('Stick Bop! < TEST >')
 
     # display background image
-    background_image = pygame.image.load('stick_pic.png').convert()
+    background_image = pygame.image.load(path.join(img_dir, 'stick_pic.png')).convert()
     screen.blit(background_image, [0, 0])
 
     # loop until user presses close button
@@ -44,21 +46,21 @@ def main():
             # change image when key(left arrow) is pressed -- image stays after keypress
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    pygame.mixer.music.load('sword_ahhhh.wav')
+                    pygame.mixer.music.load(path.join(snd_dir, 'sword_ahhhh.wav'))
                     pygame.mixer.music.set_volume(1)
                     pygame.mixer.music.play(0)
 
-                    image2 = pygame.image.load('stick_pic_sword.png').convert()
+                    image2 = pygame.image.load(path.join(img_dir, 'stick_pic_sword.png')).convert()
                     screen.blit(image2, [0, 0])
 
             # change image when key(left arrow) is released
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
-                    background_image = pygame.image.load('stick_pic.png').convert()
+                    background_image = pygame.image.load(path.join(img_dir, 'stick_pic.png')).convert()
                     screen.blit(background_image, [0, 0])
-                    
+             
         # display score to screen into top right corner           
-        game_font   = pygame.font.Font('OpenSans-Regular.ttf', 30)
+        game_font   = pygame.font.Font(path.join(font_dir, 'OpenSans-Regular.ttf'), 30)
         score_text  = 'Score: ' + str(score)
         score_label = game_font.render(score_text, 1, brown)
         screen.blit(score_label, (SCREEN_WIDTH - 180, SCREEN_HEIGHT - (SCREEN_HEIGHT - 20)))
