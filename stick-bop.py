1317715c597b170a87c8ef73a3a03523e2c364bf
jwl-7 <lucidrainbows@gmail.com>
2018-12-20 00:18:33 -0500

    added main menu to game

--- stick-bop.py
+++ stick-bop.py
@@ -31,51 +31,93 @@ ORANGE = (252, 151, 105)
 PURPLE = (171, 157, 244)
 BLUE   = (119, 220, 230)
 BLACK  = (  0,   0,   0)
-    
-def main():
+
+
+def game_init():
     pygame.init()
-    size   = SCREEN_WIDTH, SCREEN_HEIGHT
+    pygame.mixer.init()
+
+def game_menu():
+    size = SCREEN_WIDTH, SCREEN_HEIGHT
     screen = pygame.display.set_mode(size)
-    clock  = pygame.time.Clock()
-    pygame.display.set_caption('Stick Bop! < TEST >')
+    menu_snd = pygame.mixer.music.load(path.join(SND_DIR, 'piano-lofi-rain.ogg'))
+    pygame.mixer.music.play(-1)
+
+    menu_img = pygame.image.load(path.join(IMG_DIR, 'game-menu.png')).convert()
+    menu_img = pygame.transform.scale(menu_img, (SCREEN_WIDTH, SCREEN_HEIGHT), screen)
+
+    screen.blit(menu_img, [0, 0])
+    pygame.display.update()
+
+    while True:
+        event = pygame.event.poll()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                break
+            elif event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                quit()
+        elif event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        else:
+            pygame.display.update()
+
+    print('GAME READY TO START')
+
+def main():
+    game_init()
+
+    # setup game window
+    size = SCREEN_WIDTH, SCREEN_HEIGHT
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+    pygame.display.set_caption('Stick Bop!')
 
     # load assets
-    background_image = pygame.image.load(path.join(IMG_DIR, 'stick-bop-menu.png')).convert()
-    pygame.mixer.music.load(path.join(SND_DIR, 'sword_ahhhh.wav'))
-    image2 = pygame.image.load(path.join(IMG_DIR, 'stick_pic_sword.png')).convert()
+    image1 = pygame.image.load(path.join(IMG_DIR, 'stick-pic.png')).convert()
+    image2 = pygame.image.load(path.join(IMG_DIR, 'stick-pic-sword.png')).convert()
     game_font = pygame.font.Font(path.join(FONT_DIR, 'OpenSans-Regular.ttf'), 30)
 
-    # display background image
-    screen.blit(background_image, [0, 0])
+    # MAIN GAME LOOP
+    #----------------------------------------------------------------
 
     # loop until user presses close button
-    done = False
+    running = True
+    menu_display = True
 
     # program loop
-    while done == False:
+    while running:
+        if menu_display:
+            game_menu()
+            pygame.time.wait(1000)
+            pygame.mixer.music.stop()
+            menu_display = False
+
+        clock.tick(FPS)
+        screen.blit(image1, [0, 0])
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True
+                running = False
 
             # change image when key(left arrow) is pressed -- image stays after keypress
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    pygame.mixer.music.set_volume(1)
-                    pygame.mixer.music.play(0)
+                    sword_snd = pygame.mixer.Sound(path.join(SND_DIR, 'sword_ahhhh.wav'))
+                    sword_snd.play()
                     screen.blit(image2, [0, 0])
 
             # change image when key(left arrow) is released
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
-                    screen.blit(background_image, [0, 0])
+                    screen.blit(image1, [0, 0])
              
         # display score to screen into top right corner           
-        score_text  = 'Score: ' + str(SCORE)
+        score_text = 'Score: ' + str(SCORE)
         score_label = game_font.render(score_text, 1, BROWN)
         screen.blit(score_label, (SCREEN_WIDTH - 180, SCREEN_HEIGHT - (SCREEN_HEIGHT - 20)))
 
         pygame.display.update()
-        clock.tick(FPS)
 
     pygame.quit()
 
