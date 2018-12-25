97b875ba1b2c2dbd1021ff221172e613d32c2716
jwl-7 <lucidrainbows@gmail.com>
2018-12-24 19:33:35 -0500

    lightweight pygame tester file

---
+++ pygame-tester.py
@@ -0,0 +1,34 @@
+import pygame
+
+SCREEN_WIDTH  = 900
+SCREEN_HEIGHT = 700
+
+WHITE  = (253, 250, 243)
+BLACK  = ( 45,  43,  46)
+BLUE   = (119, 220, 230)
+
+def main():
+    pygame.init()
+    pygame.mixer.init()
+    size = SCREEN_WIDTH, SCREEN_HEIGHT
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Pygame Test')
+    clock = pygame.time.Clock()
+
+    running = True
+
+    while running:
+        screen.fill(WHITE)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                
+        pygame.display.update()
+        clock.tick(30)
+
+    pygame.quit()
+    quit()
+
+if __name__ == '__main__':
+    main()
